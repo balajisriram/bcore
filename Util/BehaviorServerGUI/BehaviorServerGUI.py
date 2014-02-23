e62e4b9745486b02a0349324e971366ce813350c
Balaji Sriram <balajisriram@gmail.com>
2014-02-23 09:07:02 -0800

    This section is still under a lot of flux

--- Util/BehaviorServerGUI/BehaviorServerGUI.py
+++ Util/BehaviorServerGUI/BehaviorServerGUI.py
@@ -1,30 +1,41 @@
-#from BCore.Classes.ClientAndServer.BServer import BServer
-#import Kivy
-
 from kivy.app import App
+from kivy.core.window import Window
 
-from kivy.uix.scatter import Scatter
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.listview import ListView
+from kivy.uix.button import Button
 
+from kivy.properties import ListProperty
 
-class BServerGUI(App):
-
-    def __init__(self, *args):
-        if not args:
-            self.data = None
-        else:
-            self.data = args[0]
-
+import time
+    
+class BServerApp(App):
+    def createSubjectListing(self, Subjects, Box):
+        Box.add_widget(ListView(item_strings=Subjects))
+        return Box
+        
+    def createStationListing(self, Stations, Box):
+        for Station in Stations:
+            Box.add_widget(Button(text=Station))
+        return Box
+                
+class BServerAppDefault(BServerApp):
+    Subjects = ListProperty(['Sub1','Sub2','Sub3','Sub4','Sub5'])
+    Stations = ListProperty(['Station1','Station2','Station3','Station4','Station5','Station6'])
     def build(self):
-        f = FloatLayout()
-        s = Scatter()
-        l = Label(text="Hello!",
-                font_Size=150)
-        f.add_widget(s)
-        s.add_widget(l)
-        return f
-
-
+        BServerWidget = BoxLayout(orientation='horizontal', spacing=10)
+        
+        SubjectBox = BoxLayout(orientation='vertical', spacing=1, size_hint=(0.3,1)); 
+        StationBox = GridLayout(cols=3, spacing=1, size_hint=(0.7,1)); 
+        
+        SubjectBox = self.createSubjectListing(self.Subjects, SubjectBox) 
+        StationBox = self.createStationListing(self.Stations, StationBox)
+        
+        BServerWidget.add_widget(SubjectBox)
+        BServerWidget.add_widget(StationBox)
+        
+        return BServerWidget
+        
 if __name__ == "__main__":
-    BServerGUI().run()
+    BServerAppDefault().run()
