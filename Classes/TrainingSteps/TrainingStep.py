ab9ac96aff93fa74968f318db64ef8d8887f917c
Balaji Sriram <balajisriram@gmail.com>
2014-03-03 09:36:39 -0800

    Started work on training step

--- Classes/TrainingSteps/TrainingStep.py
+++ Classes/TrainingSteps/TrainingStep.py
@@ -1,2 +1,21 @@
 class TrainingStep(object):
-    pass
+    """
+        TRAININGSTEP is added to a protocol. This will determine the behavior 
+        of the step. All training steps will follow a trial structure and
+        will need a trialManager. A Scheduler to determine when to show trials.
+        And a Criterion to determine when to graduate. 
+    """
+    name = ''
+    TrialManager = []
+    Scheduler = []
+    Criterion = []
+    
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.TrialManager = kwargs['trialManager']
+        self.Scheduler = kwargs['scheduler']
+        self.Criterion = kwargs['criterion']
+        
+    def doTrial(self, **kwargs):
+        if __debug__: 
+            assert kwargs['session'].tsOKForSessMgr, ''
