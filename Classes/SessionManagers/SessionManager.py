ce9885e6cfd35f575c81d4366f1669202751f6f4
Balaji Sriram <balajisriram@gmail.com>
2015-12-30 11:21:32 -0800

    Scheduler is renamed SessionManager

--- Classes/SessionManagers/SessionManager.py
+++ Classes/SessionManagers/SessionManager.py
@@ -1,2 +1,35 @@
 class SessionManager(object):
-    pass
+
+	ver = Ver('0.0.1')  # Feb 28 2014
+    name = ''
+
+
+    def __init__(self, **kwargs):
+    	self.name = kwargs['name']
+
+    def checkSchedule(self):
+    	return False
+    
+
+
+class NoTimeOff(SessionManager):
+
+	# no new properties defined here
+
+	def __init__(self, **kwargs):
+		super(NoTimeOff,self).__init__(**kwargs)
+
+	def checkSchedule(self):
+		return True
+
+
+class MinutesPerSession(SessionManager):
+
+	minutes = 60
+	hoursBetweenSessions = 23
+	
+	def __init__(self, **kwargs):
+		super(NoTimeOff,self).__init__(**kwargs)
+
+	def checkSchedule(self):
+		return True
