9a419a2f0556249cb24115d17411de196434168e
Balaji Sriram <balajisriram@gmail.com>
2017-02-28 07:33:23 -0800

    This is what AFCGratings needs to look like. need to work on _setupPhases

--- Classes/TrialManagers/StandardVisionBehaviorTrialManager.py
+++ Classes/TrialManagers/StandardVisionBehaviorTrialManager.py
@@ -134,6 +134,78 @@ class Gratings(StandardVisionBehaviorTrialManager):
             soundPlayed=('trialStartSound', 50))
 
 
+class AFCGratings(Gratings):
+    """
+        GRATINGS defines a standard gratings trial manager
+            PixPerCycs
+            Orientations
+            DriftFrequencies
+            Phases
+            Contrasts
+            Durations
+            Radii # in units of "Scale"
+
+            RadiusType = "HardEdge"
+            Scale = "ScaleToHeight"
+    """
+
+    PixPerCycs = [[128],[128]]
+    Orientations = [[-PI / 4], [PI / 4]]
+    DriftFrequencies = [[0],[0]]
+    Phases = [numpy.linspace(start=-PI, stop=PI, num=8, endpoint=True),numpy.linspace(start=-PI, stop=PI, num=8, endpoint=True)]
+    Contrasts = [[1],[1]]
+    Durations = [[float('Inf')],[float('Inf')]]
+
+    ver = Ver('0.0.1')
+
+    def __init__(afcgrating, **kwargs):
+        super(Gratings, grating).__init__(**kwargs)
+
+        if 'PixPerCycs'in kwargs:
+            grating.PixPerCycs = kwargs['PixPerCycs']
+        if 'Orientations' in kwargs:
+            grating.Orientations = kwargs['Orientations']
+        if 'DriftFrequencies'in kwargs:
+            grating.DriftFrequencies = kwargs['DriftFrequencies']
+        if 'Phases' in kwargs:
+            grating.Phases = kwargs['Phases']
+        if 'Contrasts'in kwargs:
+            grating.Contrasts = kwargs['Contrasts']
+        if 'Durations' in kwargs:
+            grating.Durations = kwargs['Durations']
+
+    def CalcStim(gratings, **kwargs):
+        (H, W, Hz) = gratings.ChooseResolution(kwargs)
+
+    def ChooseResolution(gratings, **kwargs):
+        pass
+
+    def _setupPhases(gratings, **kwargs):
+        """
+        Gratings:_setupPhases is a simple trialManager. It is for autopilot
+        It selects from PixPerCycs, Orientations, DriftFrequencies, Phases
+        Contrasts, Durations and shows them one at a time. There is only one
+        phase. There is no "correct" and no responses are required/recorded
+        """
+        (stimulus, stimType, resolution, hz, bitDepth, scaleFactor,
+            framesTotal) = gratings.calcStim(kwargs)
+        # Just display stim
+
+        gratings.Phases[0] = PhaseSpecs(
+            stimulus=stimulus,
+            stimType=stimType,
+            startFrame=0,
+            transitions={doNothing: 1},
+            framesUntilTransition=framesTotal,
+            autoTrigger=False,
+            scaleFactor=scaleFactor,
+            phaseType='stimulus',
+            phaseName='stim',
+            isStim=True,
+            indexPulses=False,
+            soundPlayed=('trialStartSound', 50))
+
+
 class Gratings_GaussianEdge(Gratings):
     """
         GRATINGS_GAUUIANEDGE defines a standard gratings trial manager
