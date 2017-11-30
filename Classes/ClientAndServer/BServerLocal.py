ed486430509b212c24a34ec9eee9341eb8851088
Balaji Sriram <balaji.sriram@biogen.com>
2017-11-30 15:12:55 -0500

    moving along nicely

--- Classes/ClientAndServer/BServerLocal.py
+++ Classes/ClientAndServer/BServerLocal.py
@@ -1,4 +1,4 @@
-import zmq, time, os
+import zmq, time, os, pickle
 
 from verlib import NormalizedVersion as Ver
 
@@ -13,8 +13,6 @@ class BServerLocal(object):
             subjects            : list of subjects
             assignments         : dictionary with keys being subjectID
                                 and values being list of stationIDs
-            revisionControl     : dictionary with access to details about
-                                the repository
     """
     version = Ver('0.0.1')  # Nov 7, 2017
     serverID = ''
@@ -26,12 +24,91 @@ class BServerLocal(object):
     assignments = {}
     StationConnections = {}
 
-    def __init__(server):
-        server.serverID = 0
-        server.serverDataPath = os.path.join(BCore.get_base_directory(),'BCoreData','ServerData')
-        server.serverIP = 'http://localhost'
-        server.creationTime = time.time()
+    def __init__(self):
+        self.serverID = 0
+        self.serverDataPath = os.path.join(BCore.get_base_directory(),'BCoreData','ServerData')
+        self.serverIP = 'http://localhost'
+        self.creationTime = time.time()
 
+    @staticmethod
+    def load():
+        """
+            Alias for BServerLocal.loadServer
+        """
+        return BServerLocal.load_server()
+
+    @staticmethod
+    def load_server():
+        # use standard location for path,
+        # make sure to never modify server here:
+        dbLoc = os.path.join(
+            BCore.get_base_directory(), 'BCoreData', 'ServerData', 'db.BServer')
+        if os.path.isfile(dbLoc):
+            with open(dbLoc, 'rb') as f:
+                server = pickle.load(f)
+
+            print('BServer loaded')
+        else:
+            raise RuntimeError('db.Server not found. Ensure it exists before \
+                calling loadServer')
+        return server
+
+    def save(self):
+        """
+            Alias for server.saveServer
+        """
+        self.save_server()
+
+    def save_server(self):
+        srcDir = os.path.join(
+            BCore.get_base_directory(), 'BCoreData', 'ServerData')
+        desDir = os.path.join(
+            BCore.get_base_directory(), 'BCoreData', 'ServerData', 'backupDBs')
+
+        if not os.path.isdir(self.serverDataPath):
+            # assume that these are never made alone...
+            self._setupPaths()
+
+        if os.path.isfile(os.path.join(srcDir, 'db.BServer')):  # old db exists
+            print(('Old db.Bserver found. moving to backup'))
+            old = BServer()  # standardLoad to old
+            desName = 'db_' + BCore.get_time_stamp(old.creationTime) + '.BServer'
+            shutil.copyfile(
+                os.path.join(srcDir, 'db.BServer'),  # source
+                os.path.join(desDir, desName)  # destination
+                )
+            print(('Moved to backup... deleting old copy'))
+            os.remove(os.path.join(srcDir, 'db.BServer'))
+
+        # there might be some attributes that need to be deleted
+        # delete them here before continuing
+        print(('Cleaning and pickling object'))
+        cleanedBServer = copy.deepcopy(self)
+        cleanedBServer.StationConnections = {}
+        with open(os.path.join(srcDir, 'db.BServer'), 'wb') as f:
+            pickle.dump(cleanedBServer, f)
+
+    def load_backup(self):
+        """
+            Use this only if you specifically require the deletion of current
+            db.BServer and replacement with an older backup. Only the latest
+            back up is used.
+        """
+        desDir = os.path.join(
+            BCore.get_base_directory(), 'BCoreData', 'ServerData')
+        srcDir = os.path.join(
+            BCore.get_base_directory(), 'BCoreData', 'ServerData', 'backupDBs')
+        # delete the original database
+        os.remove(os.path.join(desDir, 'db.BServer'))
+        # find the latest file in the backupDBs
+        newestBkup = max(os.listdir(srcDir), key=os.path.getctime)
+        shutil.copyfile(
+                os.path.join(srcDir, newestBkup),  # source
+                os.path.join(desDir, 'db.BServer')  # destination
+                )
+        # delete the newest backup
+        os.remove(os.path.join(srcDir, newestBkup))
+        # load the backup and return it ## TOBEDONE
 
 def _setup_paths(force_delete=False):
     if force_delete:
