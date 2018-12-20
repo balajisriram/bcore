48e83f91fb821fc1a647381f5d28c2017b229577
Balaji Sriram <balaji.sriram@biogen.com>
2018-12-20 14:28:06 -0500

    Some issues with space vs tabs

--- Users/Biogen/__init__.py
+++ Users/Biogen/__init__.py
@@ -7,11 +7,11 @@ __maintainer__ = "Balaji Sriram"
 __email__ = "balajisriram@gmail.com"
 __status__ = "Production"
 
-from BehaviorProtocols import get_behavior_protocol_biogen
-from PhysiologyProtocols import get_phys_protocol_biogen
+from .BehaviorProtocols import get_behavior_protocol_biogen
+from .PhysiologyProtocols import get_phys_protocol_biogen
 
 def get_protocol_from_name(name):
-	if name in ['orientation_tuning_biogen_08292018','ortune','short_duration_biogen_08292018','orsdp']:
+    if name in ['orientation_tuning_biogen_08292018','ortune','short_duration_biogen_08292018','orsdp']:
         return get_phys_protocol_biogen(name)
     elif name in ['lick_for_reward_biogen_09142018','lfr','classical_conditioning_protocol_12022018','ccp','auditory_go_protocol_12192018','audgo']:
         return get_behavior_protocol_biogen(name)
