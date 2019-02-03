320586efdce87786d9938cf97c22c7d426102f06
Balaji Sriram <balaji.sriram@biogen.com>
2019-02-03 11:47:10 -0500

    better location

--- Classes/TrialManagers/BaseTrialManagers.py
+++ Classes/TrialManagers/BaseTrialManagers.py
@@ -82,7 +82,7 @@ class BaseTrialManager(object):
         # text stim to denote trial
         trial_number_text = psychopy.visual.TextStim(station._window,
             text='trial_number::{0}, this_session::{1}'.format(trial_record['trial_number'], np.sum(np.asarray(compiled_record['session_number'])==trial_record['session_number'])+1), 
-            pos=(-0.95,0.95), units='norm', height=0.04,
+            pos=(-0.99,0.99), units='norm', height=0.04,
             alignHoriz='left',alignVert='top')
         
         ### loop into trial phases
