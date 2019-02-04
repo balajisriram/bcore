0e5e5649893a264857fcb95ced0000428e9ca717
Balaji Sriram <balaji.sriram@biogen.com>
2019-02-04 13:13:54 -0500

    ortune has long and short decoding!

--- Users/Biogen/PhysiologyProtocols.py
+++ Users/Biogen/PhysiologyProtocols.py
@@ -37,9 +37,21 @@ def get_orientation_tuning_protocol():
 											  reinforcement_manager=NoReinforcement(),
                                               iti=1., itl=0.),
                         session_manager=NoTimeOff(),
+                        criterion=NumTrialsDoneCriterion(num_trials=200,num_trials_mode='consecutive'))
+    ts3 = TrainingStep(name='short_duration_pm45deg_8phases',
+                       trial_manager=Gratings(name='short_duration_pm45deg_8phases',
+                                              deg_per_cycs=[0.1],
+                                              orientations=[45,-45],
+                                              contrasts=[1.,0.15],
+                                              durations=[0.05,0.1,0.15,0.2],
+											  phases=[0,0.125,0.25,0.375,0.5,0.625,0.75,0.875],
+                                              radii=[400],
+                                              reinforcement_manager=NoReinforcement(),
+                                              iti=1., itl=0.),
+                        session_manager=NoTimeOff(),
                         criterion=RepeatIndefinitely())
-    training_steps = [ts1,ts2]
-    return StartsAtOneProtocol(training_steps=training_steps, name='or_tuning_protocol_biogen_01222019')
+    training_steps = [ts1,ts2,ts3]
+    return StartsAtOneProtocol(training_steps=training_steps, name='or_tuning_protocol_biogen_02042019')
 
 def get_short_duration_protocol():
     ts1 = TrainingStep(name='or_tuning_ts_8_ors_drift_2Hz_2s_fullC',
@@ -69,8 +81,8 @@ def get_short_duration_protocol():
     training_steps = [ts1,ts2]
     return StartsAtOneProtocol(training_steps=training_steps, name='short_duration_protocol_biogen_08292018')
 
-def get_phys_protocol_biogen(name='orientation_tuning_biogen_08292018'):
-    if name in ['orientation_tuning_biogen_08292018','ortune']:
+def get_phys_protocol_biogen(name='or_tuning_protocol_biogen_02042019'):
+    if name in ['or_tuning_protocol_biogen_02042019','ortune']:
         return get_orientation_tuning_protocol()
     elif name in ['short_duration_biogen_08292018','orsdp']:
         return get_short_duration_protocol()
