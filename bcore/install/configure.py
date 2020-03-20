d143ed4d757d1b40dc6ca5180d935307173e9fde
balajisriram <balajisriram@gmail.com>
2020-03-19 22:29:14 -0400

    configuration for the installation

--- bcore/install/configure.py
+++ bcore/install/configure.py
@@ -1,25 +1,161 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-import npyscreen
+"""
+    1. Drop into BASECONFIGFORM
+"""
+import npyscreen as nps
 import json
+import os
 
-class GetConfigDetails(npyscreen.NPSApp):
-    def main(self):
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F  = npyscreen.Form(name = "Configure your BCore installation...",)
-        t  = F.add(npyscreen.TitleText, name = "Please provide data_base_path:",)
-        path = F.add(npyscreen.TitleFilenameCombo, name="PATH:")
+from bcore import get_ip_addr
 
-        mode = F.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
-                values = ["Server","Client","Stand Alone"], scroll_exit=True)
+################################################################################
+##############################       UTILITY      ##############################
+################################################################################
 
-        # This lets the user interact with the Form.
-        F.edit()
+def get_base_path():
+    # returns the directory under the BCore directory
+    base = os.path.split(os.path.abspath(__file__))
+    base = os.path.split(base[0])
+    base = os.path.split(base[0])
+    base = os.path.split(base[0])
+    return base[0]
 
+def get_database_path():
+    return os.path.join(get_base_path(),'BCoreData')
+
+def get_config_path():
+    return os.path.join(get_base_path(),'.bcore')
+
+################################################################################
+###############################       FORMS      ###############################
+################################################################################
+
+class BaseConfigForm(nps.ActionForm):
+    def create(self):
+        self.base_path = self.add(nps.TitleFilenameCombo, name="Provide data base path:",value=get_database_path(),select_dir=True,must_exist=True,confirm_if_exists=True)
+        self.bcore_mode = self.add(nps.TitleSelectOne, max_height=5, value = [1,], name="How will this BCore installation work? Pick One:",values = ["Server","Client","Stand Alone"], scroll_exit=True)
+        self.ip_addr = self.add(nps.TitleText, name="Your IP address (you can change this is the detailsed config form):",value = get_ip_addr(), scroll_exit=False,editable=False)
+        
+        self.detail_config = self.add(nps.CheckBox,name="Further configure install?")
+        
+    def on_ok(self):
+        configuration = dict()
+        configuration['base_path'] = self.base_path.value
+        configuration['mode'] = self.bcore_mode.values[self.bcore_mode.value[0]]
+        configuration['ip_addr'] = self.ip_addr.value
+        
+        configuration_text = "base_path::{0}\nmode::{1}\n".format(self.base_path.value,self.bcore_mode.values[self.bcore_mode.value[0]])
+        if self.detail_config.value:
+            configuration_text = configuration_text+'Will move to next form as we further configure the installation\n'
+            if configuration['mode']=='Server':self.parentApp.setNextForm('SERVER_CONFIG')
+            elif configuration['mode']=='Client':self.parentApp.setNextForm('CLIENT_CONFIG')
+            else:self.parentApp.setNextForm('STANDALONE_CONFIG')
+        else:
+            self.parentApp.setNextForm(None)
+            
+        nps.notify_confirm('Saving configuration at {0}\n\n{1}'.format(get_config_path(),configuration_text),'OK Button',editw=1,wide=True)
+        self.parentApp.basic_config = configuration
+        
+        
+        
+    def on_cancel(self):
+        nps.notify_confirm("No changes have been made to configuration. Re-run install/configure.py to configure your installation",'Exiting',editw=1)
+        self.parentApp.setNextForm(None)
+        
+class ConfigureServer(nps.ActionFormWithMenus):
+    def create(self):
+        self.server_name = self.add(nps.TitleText, name="Name your server",value = "server-001", scroll_exit=False,editable=True)
+
+        self.ip_addr = self.add(nps.TitleText, name="Your IP address:",value = get_ip_addr(), scroll_exit=False,editable=True)
+        self.subnet_mask = self.add(nps.TitleText, name="Your subnet mask address:",value = '255.255.255.0', scroll_exit=False,editable=True)
+        self.gateway = self.add(nps.TitleText, name="Your gateway address:",value = '192.168.0.1', scroll_exit=False,editable=True)
+        self.port = self.add(nps.TitleText, name="Your port number:",value = '5550', scroll_exit=False,editable=True)
+        
+        self.ADD_SUBJECT = self.add(AddSubjectButtonPress,name="Add a subject")
+        self.ADD_STATION = self.add(AddStationButtonPress,name="Add a station")
+                
+        
+    def on_ok(self):
+        self.parentApp.setNextForm(None)
+        
+    def on_cancel(self):
+        nps.notify_confirm("No changes have been made to configuration",'Exiting',editw=1)
+        self.parentApp.setNextForm(None)
+
+
+class ConfigureClient(nps.ActionForm):
+    def create(self):
+        pass
+        
+    def on_ok(self):
+        self.parentApp.setNextForm(None)
+        
+    def on_cancel(self):
+        nps.notify_confirm("No changes have been made to configuration",'Exiting',editw=1)
+        self.parentApp.setNextForm(None)
+
+
+class ConfigureStandAlone(nps.ActionForm):
+    def create(self):
+        pass
+        
+    def on_ok(self):
+        self.parentApp.setNextForm(None)
+        
+    def on_cancel(self):
+        nps.notify_confirm("No changes have been made to configuration",'Exiting',editw=1)
+        self.parentApp.setNextForm(None)
+
+
+class AddSubject(nps.ActionForm):
+    def create(self):
+        pass
+        
+    def on_ok(self):
+        self.parentApp.switchFormPrevious()
+        
+    def on_cancel(self):
+        nps.notify_confirm("No changes have been made to configuration",'Exiting',editw=1)
+        self.parentApp.switchFormPrevious()
+       
+class AddStation(nps.ActionForm):
+    def create(self):
+        pass
+        
+    def on_ok(self):
+        self.parentApp.switchFormPrevious()
+        
+    def on_cancel(self):
+        nps.notify_confirm("No changes have been made to configuration",'Exiting',editw=1)
+        self.parentApp.switchFormPrevious()
+################################################################################
+##############################       WIDGETS      ##############################
+################################################################################ 
+
+class AddSubjectButtonPress(nps.ButtonPress):
+    def whenPressed(self):
+        self.parent.parentApp.switchForm('ADD_SUBJECT')
+
+class AddStationButtonPress(nps.ButtonPress):
+    def whenPressed(self):
+        self.parent.parentApp.switchForm('ADD_STATION')
+
+
+################################################################################
+################################       APP      ################################
+################################################################################
+
+class ConfigureBCore(nps.NPSAppManaged):
+    basic_config = []
+    
+    def onStart(self):
+        self.addForm('MAIN',BaseConfigForm,name="Configure your BCore installation")
+        self.addForm('SERVER_CONFIG',ConfigureServer,name="Configure your Server")
+        self.addForm('CLIENT_CONFIG',ConfigureClient,name="Configure your Client")
+        self.addForm('STANDALONE_CONFIG',ConfigureStandAlone,name="Configure StandAlone install")
+        self.addForm('ADD_SUBJECT',AddSubject,name="Add a subject")
+        self.addForm('ADD_STATION',AddStation,name="Add a station")
+        
 
 if __name__ == "__main__":
-    App = GetConfigDetails()
+    App = ConfigureBCore()
     out = App.run()
-    print(out)
